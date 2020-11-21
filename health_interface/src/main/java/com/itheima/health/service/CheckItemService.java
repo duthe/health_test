21fe006dca7ae34d7da0e818f5ffc5cd0beafcdd
ado <du.do@outlook.com>
2020-11-21 20:33:56 +0800

    上传项目

---
+++ health_interface/src/main/java/com/itheima/health/service/CheckItemService.java
@@ -0,0 +1,12 @@
+package com.itheima.health.service;
+
+import com.itheima.health.pojo.CheckItem;
+
+import java.util.List;
+
+public interface CheckItemService {
+
+    List<CheckItem> findAll();
+
+    void add(CheckItem checkItem);
+}
---
+++ health_web/src/main/java/com/itheima/health/controller/CheckItemController.java
@@ -0,0 +1,34 @@
+package com.itheima.health.controller;
+
+import com.alibaba.dubbo.config.annotation.Reference;
+import com.itheima.health.constant.MessageConstant;
+import com.itheima.health.entity.Result;
+import com.itheima.health.pojo.CheckItem;
+import com.itheima.health.service.CheckItemService;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/checkitem")
+public class CheckItemController {
+    @Reference
+    private CheckItemService checkItemService;
+
+
+    @RequestMapping("/findAll")
+    public Result findAll() {
+        List<CheckItem> checkItemList = checkItemService.findAll();
+        return new Result(true, MessageConstant.QUERY_CHECKITEM_SUCCESS, checkItemList);
+    }
+
+
+    @RequestMapping("add")
+    public Result add(@RequestBody CheckItem checkItem) {
+        checkItemService.add(checkItem);
+        return new Result(true, MessageConstant.ADD_CHECKITEM_SUCCESS);
+    }
+
+}
