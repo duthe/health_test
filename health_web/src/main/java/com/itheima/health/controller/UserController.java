41f85c8892a3996408456b2318b1e0034468cf1c
ado <du.do@outlook.com>
2020-11-30 17:16:07 +0800

    异步登录 信息提示

--- health_web/src/main/java/com/itheima/health/controller/UserController.java
+++ health_web/src/main/java/com/itheima/health/controller/UserController.java
@@ -3,7 +3,7 @@ package com.itheima.health.controller;
 import com.itheima.health.constant.MessageConstant;
 import com.itheima.health.entity.Result;
 import org.springframework.security.core.context.SecurityContextHolder;
-import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.User;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -14,8 +14,9 @@ public class UserController {
     @RequestMapping("/findUserName")
     public Result findUserName() {
         // 获取登陆用户的认证信息
-        UserDetails userDetails = (UserDetails) SecurityContextHolder.getContext().getAuthentication().getPrincipal();
+        User userDetails = (User) SecurityContextHolder.getContext().getAuthentication().getPrincipal();
         // 登陆用户名
+
         String username = userDetails.getUsername();
 
         return new Result(true, MessageConstant.GET_USERNAME_SUCCESS,username);
--- health_web/src/main/java/com/itheima/health/security/SpringSecurityService.java
+++ health_web/src/main/java/com/itheima/health/security/SpringSecurityService.java
@@ -10,12 +10,11 @@ import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
-import org.springframework.stereotype.Component;
+
 
 import java.util.ArrayList;
 import java.util.Set;
 
-@Component
 public class SpringSecurityService implements UserDetailsService {
 
     @Reference
